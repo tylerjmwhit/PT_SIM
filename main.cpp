e04334b7697bd6e32c9dbaf44325f787118b15df
tylerjmwhit <tylerjmwhit@gmail.com>
2022-12-08 17:10:32 -0800

    Committing changes to add part A and B function

--- main.cpp
+++ main.cpp
@@ -29,6 +29,11 @@ struct pageTable{
 	}
 };
 
+struct clockS{
+	page* currentPage;
+	int position;
+};
+
 pageTable create_table(char* filename){
 	pageTable vtable;
 	ifstream inputFile;
@@ -49,30 +54,83 @@ pageTable create_table(char* filename){
 	return vtable;
 }
 
-void translate_address(pageTable tablein){
-	string tempinput;
+void translate(string& tempinput, int& off, int& location, int& output, const pageTable& tablein){
 	int temp_int;
 	string binary;
-	string offset;
 	string pnumber;
+	string offset;
+
+	temp_int = stoi(tempinput, nullptr, 0);
+	binary = bitset<16>(temp_int).to_string();
+	offset = binary.substr(off);
+	pnumber = binary.substr(0,off);
+	location = stoi(pnumber, nullptr,2);
+	output = stoi(tablein.table.at(location).bframe + offset, nullptr,2);
+}
+
+void translate_address_B(pageTable tablein){
+	clockS clock;
+	clock.currentPage = &tablein.table[0];
+	clock.position = 0;
+	string tempinput;
+	string offset;
 	int output;
-	int temp1;
+	int location;
 	int off = 16 - log2(tablein.size);
 	while(getline(cin,tempinput)){
-		temp_int = stoi(tempinput, nullptr, 0);
-		binary = bitset<16>(temp_int).to_string();
-		//cout <<binary <<endl;
-		offset = binary.substr(off);
-		//cout <<offset <<endl;
-		pnumber = binary.substr(0,off);
-		//cout <<pnumber <<endl;
-		int temp1 = stoi(pnumber, nullptr,2);
-		//cout << temp1 << endl;
-		if(tablein.table.at(temp1).valid){
-			output = stoi(tablein.table.at(temp1).bframe + offset, nullptr,2);
+		translate(tempinput, off, location, output, tablein);
+		if(tablein.table.at(location).valid){
+			tablein.table.at(location).LRU = 1;
 			cout << output <<endl;
 		}
-		else if(tablein.table.at(temp1).perm){
+		else if(tablein.table.at(location).perm){
+			cout << "Page Fault ";
+			while(tablein.table.at(location).valid == 0){
+				if(clock.currentPage->valid == 0){
+					clock.position++;
+					if(clock.position == tablein.table.size())
+						clock.position = 0;
+					clock.currentPage = &tablein.table[clock.position];
+				}
+				else if(clock.currentPage->LRU == 1){
+					clock.currentPage->LRU = 0;
+					clock.position++;
+					if(clock.position == tablein.table.size())
+						clock.position = 0;
+					clock.currentPage = &tablein.table[clock.position];
+				}
+				else{
+					clock.currentPage->valid = 0;
+					tablein.table.at(location).valid = 1;
+					tablein.table.at(location).frame = clock.currentPage->frame;
+					tablein.table.at(location).bframe = clock.currentPage->bframe;
+					tablein.table.at(location).LRU = 1;
+					clock.position++;
+					if(clock.position == tablein.table.size())
+						clock.position = 0;
+					clock.currentPage = &tablein.table[clock.position];
+				}
+			}
+			translate(tempinput, off, location, output, tablein);
+			cout << output <<endl;
+		}
+		else{
+			cout << "SEGFAULT" <<endl;
+		}
+	}
+}
+
+void translate_address_A(pageTable tablein){
+	string input;
+	int output;
+	int location;
+	int off = 16 - log2(tablein.size);
+	while(getline(cin,input)){
+		translate(input, off, location, output, tablein);
+		if(tablein.table.at(location).valid){
+			cout << output <<endl;
+		}
+		else if(tablein.table.at(location).perm){
 			cout << "Disk" <<endl;
 		}
 		else{
@@ -82,14 +140,21 @@ void translate_address(pageTable tablein){
 }
 
 int main(int argc, char * argv[]) {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf ("not enough arguments");
 		exit (1);
 	}
 	pageTable vtable = create_table(argv[1]);
-	translate_address(vtable);
-
-
+	if(*argv[2] == 'A'){
+		translate_address_A(vtable);
+	}
+	else if(*argv[2] == 'B'){
+		translate_address_B(vtable);
+	}
+	else{
+		cerr << "please enter 'A' or 'B' as second parameter to specify mode."<<endl;
+		exit(1);
+	}
 	return 0;
 }
